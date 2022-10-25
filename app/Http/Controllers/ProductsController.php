eeb1b88b7563466c756745d59d5aec0aac96fbc8
Hector Luis Bolaño Alvarado <helboal15@outlook.com>
2022-10-25 15:42:08 -0500

    Se completa el método show del controlador de productos.

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -73,9 +73,11 @@ class ProductsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Product $product)
     {
-        //
+        $response = new ProductResource($product);
+        
+        return $this->successResponse($response);
     }
 
     /**

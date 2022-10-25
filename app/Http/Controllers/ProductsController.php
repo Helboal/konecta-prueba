06627963986abb554c702990f7af4581a46abbe1
Hector Luis Bolaño Alvarado <helboal15@outlook.com>
2022-10-25 16:24:05 -0500

    Se completa el método update del controlador de productos.

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Requests\Product\StoreProductRequest;
+use App\Http\Requests\Product\UpdateProductRequest;
 
 class ProductsController extends Controller
 {
@@ -64,7 +65,7 @@ class ProductsController extends Controller
         }
         catch(Exception $e) {
             return $this->errorResponse('Error al crear el producto, por favor intente nuevamente.', $e->getMessage(), 422);
-        }              
+        }
     }
 
     /**
@@ -89,7 +90,7 @@ class ProductsController extends Controller
     public function edit(Product $product)
     {
         $categories = Category::select('id', 'category')->get();
-                
+
         return $this->successResponse([
             'categories'    => $categories,
             'product'       => $product
@@ -103,9 +104,25 @@ class ProductsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UpdateProductRequest $request, Product $product)
     {
-        //
+        try {
+            $product->update([
+                'name'          => $request->name,
+                'reference'     => $request->reference,
+                'price'         => $request->price,
+                'weight'        => $request->weight,
+                'stock'         => $request->stock,
+                'category_id'   => $request->category,
+            ]);
+
+            $response = new ProductResource($product);
+
+            return $this->successResponse($response);
+        }
+        catch(Exception $e) {
+            return $this->errorResponse('Error al actualizar el producto, por favor intente nuevamente.', $e->getMessage(), 422);
+        }
     }
 
     /**

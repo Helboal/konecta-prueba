919c5104ea3e3ae91b3b91405c71b02c8f4445a0
Hector Luis Bolaño Alvarado <helboal15@outlook.com>
2022-10-25 12:29:26 -0500

    Se crea la relación con la tabla categorias.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,4 +21,9 @@ class Product extends Model
     {
         return $this->hasMany(OrderDetail::class);
     }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }

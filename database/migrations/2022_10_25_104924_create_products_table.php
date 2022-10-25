a554ae205cd438c44c5d391bc134d78cd14755c9
Hector Luis Bolaño Alvarado <helboal15@outlook.com>
2022-10-25 11:00:57 -0500

    Se crean las migraciones de la estructura inicial de la base de datos.

---
+++ database/migrations/2022_10_25_104924_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('reference');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('weight');
+            $table->unsignedInteger('stock');
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->on('categories')->references('id')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}

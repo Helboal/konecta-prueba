7efcec3d316b4d453b77a4ec3c51d19a31c59d9c
Hector Luis Bolaño Alvarado <helboal15@outlook.com>
2022-10-31 10:44:47 -0500

    Se completa la vista del reporte para los productos con mayor stock.

--- public/js/ReportsMoreStock.js
+++ public/js/ReportsMoreStock.js
@@ -11,8 +11,71 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+/* harmony import */ var vuex__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vuex */ "./node_modules/vuex/dist/vuex.esm.js");
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  name: "ReportsMoreStock"
+  name: "ReportsMoreStock",
+  data: function data() {
+    return {
+      products: [],
+      headers: [{
+        key: 'id',
+        label: 'Id',
+        "class": 'text-center'
+      }, {
+        key: 'name',
+        label: 'Nombre',
+        "class": 'text-center'
+      }, {
+        key: 'reference',
+        label: 'Referencia',
+        "class": 'text-center'
+      }, {
+        key: 'price',
+        label: 'Precio',
+        "class": 'text-center'
+      }, {
+        key: 'weight',
+        label: 'Peso',
+        "class": 'text-center'
+      }, {
+        key: 'stock',
+        label: 'Stock',
+        "class": 'text-center'
+      }, {
+        key: 'category',
+        label: 'Categoría',
+        "class": 'text-center'
+      }, {
+        key: 'created',
+        label: 'Creado',
+        "class": 'text-center'
+      }]
+    };
+  },
+  mounted: function mounted() {
+    this.getProducts();
+  },
+  computed: _objectSpread({}, (0,vuex__WEBPACK_IMPORTED_MODULE_0__.mapGetters)({
+    overlay: 'overlay'
+  })),
+  methods: _objectSpread(_objectSpread({}, (0,vuex__WEBPACK_IMPORTED_MODULE_0__.mapMutations)({
+    setOverlay: 'SET_OVERLAY'
+  })), {}, {
+    getProducts: function getProducts() {
+      var _this = this;
+      this.setOverlay(true);
+      axios.get('reports/max-stock').then(function (response) {
+        _this.products = response.data.data;
+        _this.setOverlay(false);
+      })["catch"](function (error) {
+        reject(error);
+      });
+    }
+  })
 });
 
 /***/ }),
@@ -31,7 +94,28 @@ __webpack_require__.r(__webpack_exports__);
 var render = function render() {
   var _vm = this,
     _c = _vm._self._c;
-  return _c("div", [_vm._v("\n    moreStockReport\n")]);
+  return _c("div", {
+    staticClass: "my-5"
+  }, [_c("h1", [_vm._v("Producto(s) con mayor stock")]), _vm._v(" "), _c("b-table-lite", {
+    attrs: {
+      responsive: "",
+      small: "",
+      hover: "",
+      items: _vm.products,
+      fields: _vm.headers
+    },
+    scopedSlots: _vm._u([{
+      key: "cell(price)",
+      fn: function fn(data) {
+        return [_vm._v("\n            " + _vm._s(_vm.formatNumber(data.item.price)) + "\n        ")];
+      }
+    }, {
+      key: "cell(created)",
+      fn: function fn(data) {
+        return [_vm._v("\n            " + _vm._s(_vm.formatDate(data.item.created)) + "\n        ")];
+      }
+    }])
+  })], 1);
 };
 var staticRenderFns = [];
 render._withStripped = true;
--- resources/js/components/reports/MoreStockReport.vue
+++ resources/js/components/reports/MoreStockReport.vue
@@ -1,10 +1,59 @@
 <template>
-    <div>
-        moreStockReport
+    <div class="my-5">
+        <h1>Producto(s) con mayor stock</h1>
+        <b-table-lite responsive small hover :items="products" :fields="headers">
+            <template v-slot:cell(price)="data">
+                {{ formatNumber(data.item.price) }}
+            </template>
+            <template v-slot:cell(created)="data">
+                {{ formatDate(data.item.created) }}
+            </template>
+        </b-table-lite>
     </div>
 </template>
 <script>
+
+import { mapGetters, mapMutations } from 'vuex';
+
 export default {
-    name: "ReportsMoreStock"
+    name: "ReportsMoreStock",
+    data() {
+        return {
+            products: [],
+            headers: [
+                { key: 'id', label:'Id', class: 'text-center' },
+                { key: 'name', label:'Nombre', class: 'text-center'},
+                { key: 'reference', label:'Referencia', class: 'text-center'},
+                { key: 'price', label:'Precio', class: 'text-center'},
+                { key: 'weight', label:'Peso', class: 'text-center'},
+                { key: 'stock', label:'Stock', class: 'text-center'},
+                { key: 'category', label:'Categoría', class: 'text-center'},
+                { key: 'created', label:'Creado', class: 'text-center'},
+            ],
+        }
+    },
+    mounted() {
+        this.getProducts();
+    },
+    computed : {
+        ...mapGetters({
+            overlay: 'overlay'
+        })
+    },
+    methods: {
+        ...mapMutations({
+            setOverlay: 'SET_OVERLAY'
+        }),
+        getProducts() {
+            this.setOverlay(true);
+            axios.get('reports/max-stock').then((response) => {
+                this.products =  response.data.data;
+                this.setOverlay(false);
+            })
+            .catch((error) => {
+                reject(error)
+            })
+        }
+    }
 }
 </script>

0538f350621b289f35f22c035b92dd772af3dbfb
Hector Luis Bolaño Alvarado <helboal15@outlook.com>
2022-10-31 09:08:17 -0500

    Se crea la estructura del controlador para reportes con sus rutas.

---
+++ app/Http/Controllers/ReportsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    public function maxStockProducts()
+    {
+        
+    }
+    
+    public function bestSellerProducts()
+    {
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
-use App\Http\Controllers\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ReportsController;
 use App\Http\Controllers\ProductsController;
-
 
 Route::resource('products', ProductsController::class);
 Route::post('order', [OrderController::class, 'newOrder']);
+
+route::group(['prefix' => 'reports'], function() {
+    route::get('max-stock', [ReportsController::class, 'maxStockProducts']);
+    route::get('best-seller', [ReportsController::class, 'bestSellerProducts']);
+});

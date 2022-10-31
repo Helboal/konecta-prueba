95254951bd922604c4315805cff130ebcd11c770
Hector Luis Bolaño Alvarado <helboal15@outlook.com>
2022-10-31 09:34:47 -0500

    Se completa el método maxStockProducts para retornar los productos con mayor stock.

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductCollection;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ReportsController extends Controller
 {
     public function maxStockProducts()
     {
+        $products =  Product::where('stock', Product::max('stock'))->get();
+        
+        $response = new ProductCollection($products);
+
+        return $response;
         
     }
     
